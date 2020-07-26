7e0ae28c36e9aa9072e15cfbd063ea26e682767b
Bamdad Sabbagh <git@bamdadsabbagh.com>
2020-07-26 22:25:04 +0200

    fix: date is now printing local time instead of UTC [force-release]

--- src/scripts/components/utils.js
+++ src/scripts/components/utils.js
@@ -76,3 +76,19 @@ export const getMaxDimensions = (width, height, canvasWidth, canvasHeight, margi
     return dimensions
  
 }
+
+export const getDate = () => {
+
+    const test = new Date ()
+    const diff = -test.getTimezoneOffset () * 60 * 1000 // example UTC+2: returns -(-120)
+    const now = new Date (Date.now () + diff)
+    const iso = now.toISOString ()
+    const date = iso.slice (0, 10)
+    const time = iso.slice (11, 19)
+
+    return {
+        date,
+        time,
+    }
+
+}
